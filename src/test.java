4563190e766fc1f57d4b4445a0a9ef66b1c46157
Süpriz <Spriz@Yaykan.fritz.box>
2015-01-11 15:04:30 +0100

    Selam

--- src/test.java
+++ src/test.java
@@ -4,6 +4,7 @@ public class test {
 	public static void main(String[] args) {
 		System.out.println("Wenn ihr das Sieht sagt mir bitte bescheid von Furkan");
 		System.out.println("Selamu Aleykum Hocam");
+		System.out.println("Eyvallah");
 	}
 
 }
