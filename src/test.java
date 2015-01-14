4898c1963c8e04b4fe16c0cf87149a33f5c774fe
furkanyucel <furkanyucel@192.168.2.104>
2015-01-14 23:44:06 +0100

    test

--- src/test.java
+++ src/test.java
@@ -1,8 +1,21 @@
 
 public class test {
+	
+	public test(String title){
 
-	public static void main(String[] args) {
-		System.out.println("Wenn ihr das Sieht sagt mir bitte bescheid von Furkan");
+		JFrame frame = new JFrame(title);
+		frame.getContentPane().setLayout(new BorderLayout());
+		JLabel label = new JLabel();
+		label.setText(ausgabe.getString());
+		frame.getContentPane().add(label, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		frame.setSize(600,600);
+		System.out.println("Selamu Aleykum Hocam");
+		System.out.println("Eyvallah");
 	}
+
 
 }
