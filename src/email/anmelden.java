72fb57b47d4f2fcc96fcd7de075f25858642596d
furkanyucel <furkanyucel@10.53.167.83>
2015-01-15 18:04:57 +0100

    getter and setter methods updated

--- src/email/anmelden.java
+++ src/email/anmelden.java
@@ -1,37 +1,69 @@
 package email;
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
-import javax.swing.plaf.basic.BasicBorders.RadioButtonBorder;
 public class anmelden {
 	
 	private JDialog fenster = new JDialog();
 	static String title = "Java E-Mail-Client Connection Init Beta v.01";
-	public String ssmtp,simap,semail;
+	private String ssmtp;
+	public String getSsmtp() {
+		return ssmtp;
+	}
+
+	public void setSsmtp(String ssmtp) {
+		this.ssmtp = ssmtp;
+	}
+
+	public String getSimap() {
+		return simap;
+	}
+
+	public void setSimap(String simap) {
+		this.simap = simap;
+	}
+
+	public String getSemail() {
+		return semail;
+	}
+
+	public void setSemail(String semail) {
+		this.semail = semail;
+	}
+
+	public char[] getSpass() {
+		return spass;
+	}
+
+	public void setSpass(char[] spass) {
+		this.spass = spass;
+	}
+
+	private String simap;
+	private String semail;
 	char[] spass;
 	
 	public anmelden(String title){
 		
 		// Textfield SMTP
 		JTextField smtp = new JTextField("Please enter SMTP server", 25);
-		ssmtp = smtp.getText();
+		setSsmtp(smtp.getText());
 		
 		
 		// Textfield IMAP
 		JTextField imap = new JTextField("Please enter IMAP server", 25);
-		simap = imap.getText();
+		setSimap(imap.getText());
 		
 		// Textfield Email
 		JTextField email = new JTextField("Please enter E-Mail Adress");
-		semail = email.getText();
+		setSemail(email.getText());
 		
 		// Passwordfield
 		JPasswordField pass = new JPasswordField("",15);
-		spass = pass.getPassword();
+		setSpass(pass.getPassword());
 		
 		// Remember me 
 		final JRadioButton remember = new JRadioButton("Remember me!");
@@ -58,21 +90,6 @@ public class anmelden {
 		System.exit(0);
 		
 	}
-	
-	public String getImap(){
-		return simap;
-	}
-	
-	public String getSmtp(){
-		return ssmtp;
-	}
-	
-	public String getEmail(){
-		return semail;
-	} 
-	
-	public String getPass(){
-		return String.valueOf(spass);
-	}
+
 	
 }
