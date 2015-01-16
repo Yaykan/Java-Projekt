f0c909363e4463be659e4c661bd64ac7641fb9f0
furkanyucel <furkanyucel@192.168.2.104>
2015-01-16 23:09:43 +0100

    save wurde erfolgreich erstellt

--- src/email/save.java
+++ src/email/save.java
@@ -2,6 +2,7 @@ package email;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -9,7 +10,7 @@ import java.io.PrintWriter;
 
 public class save {
 	public save(String smtp, String imap,String email, String password) {
-		
+		char [] temp;
 		// Create file
 		try {
 			 
@@ -23,6 +24,24 @@ public class save {
 	 
 	    	} catch (IOException e) {
 		      e.printStackTrace();
+		}
+		
+		try {
+			FileOutputStream write = new FileOutputStream("text.txt");
+			temp = smtp.toCharArray();
+			int a;
+			for (int i = 0; i < temp.length; i++) {
+				a = temp[i];
+			
+				int c;
+				while ((c = a) != -1) {
+					write.write(c);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException  e) {
+			e.printStackTrace();
 		}
 		
 	}
